2911727c4e11516e94ce1554fe4185f2d6349dbf
Andrea Censi <andrea@cds.caltech.edu>
2010-12-28 12:22:29 -0800

    Fixes for numpy 1.4

--- src/cbc/tools/points_alignment_test.py
+++ src/cbc/tools/points_alignment_test.py
@@ -5,6 +5,7 @@ from snp_geometry import random_rotation, random_directions
 
 from .points_alignment import (find_best_orthogonal_transform,
                                overlap_error_after_orthogonal_transform)
+from snp_geometry.utils import assert_allclose
 
 N = 100
     
@@ -16,9 +17,9 @@ class UtilsTests(unittest.TestCase):
             R = random_rotation()
             Y = np.dot(R, X)
             Rest = find_best_orthogonal_transform(X, Y)
-            np.testing.assert_allclose(R, Rest)
+            assert_allclose(R, Rest)
             Rest2 = find_best_orthogonal_transform(Y, X)
-            np.testing.assert_allclose(R.T, Rest2)
+            assert_allclose(R.T, Rest2)
             
     def test_overlap_error(self):
         for i in range(N): #@UnusedVariable
@@ -27,7 +28,7 @@ class UtilsTests(unittest.TestCase):
             Y = np.dot(R, X)
             e1 = overlap_error_after_orthogonal_transform(X, Y)
             atol = 1E-7
-            np.testing.assert_allclose(e1, 0, atol=atol)
+            assert_allclose(e1, 0, atol=atol)
             e2 = overlap_error_after_orthogonal_transform(Y, X)
-            np.testing.assert_allclose(e2, 0, atol=atol)
+            assert_allclose(e2, 0, atol=atol)
              
